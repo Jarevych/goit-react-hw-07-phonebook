a8be989be8937dbcf2c64a3cdcc425f1333b8c70
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-10-29 12:48:14 +0200

    init, thunk

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import InputName from './InputName';
 import InputNumber from './InputNumber';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from 'redux/ContactSlice';
+import { FetchContacts, addContact } from 'redux/ApiHandler';
 import { nanoid } from 'nanoid';
 
 
@@ -11,18 +11,22 @@ export default function FormInput() {
   const [number, setNumber] = useState('');
   const contacts = useSelector(state => state.contacts.contacts)
   const dispatch = useDispatch();
+  const contactsArr = Array.isArray(contacts) ? contacts : [];
+
 
   const handleSubmit = event => {
     event.preventDefault();
     const newContact = {  id: nanoid(), name, number };
-    const contactExists = contacts.some((contact) => contact.name.toLowerCase() === name.toLowerCase() || contact.number === number);
+    const contactExists = contactsArr.some((contact) => contact.name.toLowerCase() === name.toLowerCase() || contact.number === number);
 
         if (contactExists) {
           alert(`"${name}" is already in contacts`);
           return;
         }
 
-      dispatch(addContact(newContact));
+      dispatch(addContact(newContact)).then(()=>{
+        dispatch(FetchContacts());
+      });
     setName('');
     setNumber('');
   };
